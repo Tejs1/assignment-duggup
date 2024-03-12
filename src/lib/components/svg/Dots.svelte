98569b70af7f5b9ed4d91dd24fe95987171969d6
Tejas Thorat <tejsrelax@gmail.com>
2024-03-12 23:39:26 +0530

    Update SVG width in Dots component

--- src/lib/components/svg/Dots.svelte
+++ src/lib/components/svg/Dots.svelte
@@ -3,7 +3,7 @@
 	export { className as class };
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" width="22.4" height="5.6" class={className}>
+<svg xmlns="http://www.w3.org/2000/svg" width="23" height="5.6" class={className}>
 	<circle cx="2.8" cy="2.8" r="2.8" fill="#A5B9D3" />
 	<circle cx="11.2" cy="2.8" r="2.8" fill="#A5B9D3" />
 	<circle cx="19.6" cy="2.8" r="2.8" fill="#A5B9D3" />
